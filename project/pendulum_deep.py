47b5e5bf3d23c30e46766628a7bdc597e8f0b36d
Zach <zvincent@nd.edu>
2023-12-03 17:01:53 -0500

    possible successful train with discretized action space

--- project/pendulum_deep.py
+++ project/pendulum_deep.py
@@ -6,7 +6,7 @@ import utils
 if __name__ == '__main__':
     # training parameters
     load_checkpoint = False
-    n_games = 30
+    n_games = 500
 
 
     # object creation
@@ -15,10 +15,10 @@ if __name__ == '__main__':
                      epsilon    = 1.0,
                      lr         = 0.001,
                      input_dims = (3,), #env.observation_space.shape,
-                     n_actions  = 1, #env.action_space.shape[0],
+                     n_actions  = 10, #env.action_space.shape[0],
                      mem_size   = 30000,
                      eps_min    = 0.1,
-                     batch_size = 64,
+                     batch_size = 8,
                      replace    = 1000,
                      eps_dec    = 1e-5,
                      dir        = 'models/',
@@ -39,14 +39,15 @@ if __name__ == '__main__':
         # start new episode
         score = 0
         
-        # observation has shape (3, 1), i.e. [[x][x][x]]
+        # observation is numpy.ndarray with shape (3,)
         observation, info = env.reset()
         terminated, truncated = False, False
 
         # begin training
         while not terminated and not truncated:
             action = agent.choose_action(observation)
-            observation_, reward, terminated, truncated, info = env.step([action])
+
+            observation_, reward, terminated, truncated, info = env.step([(action-5)*.2])
             score += reward
 
             # learn if we're not just evaluating the net
@@ -73,8 +74,7 @@ if __name__ == '__main__':
             best_score = avg_score
 
         # print updates
-        if i % 50 == 0:
-            print(f'GAME {i}: Epsilon {agent.epsilon}, Avg. Score: {np.mean(scores_array[-50:])}')
+        print(f'GAME: {i},\tEpsilon: {agent.epsilon:7.2f}, Score: {score:7.2f} Avg. Score: {np.mean(scores_array[-50:]):7.2f}')
 
         epsilon_array.append(agent.epsilon)
 
